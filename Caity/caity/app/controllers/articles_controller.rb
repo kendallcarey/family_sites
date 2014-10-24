64d72a21cf21724d7057d2f1cf4bc209e256468b
Kendall <kendall.carey@gmail.com>
2014-10-24 13:21:49 -0700

    fixed bugs in blog

--- Caity/caity/app/controllers/articles_controller.rb
+++ Caity/caity/app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -21,9 +22,12 @@ def show
   @article = Article.find(params[:id])
 end
 
+def edit
+  @article = Article.find(params[:id])
+end
+
 def update
   @article = Article.find(params[:id])
-
   if @article.update(article_params)
     redirect_to @article
   else
