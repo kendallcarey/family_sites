e3374088ffe10fc9b3df1154bbcbc4bf8a402f04
Kendall <kendall.carey@gmail.com>
2014-10-28 20:39:27 -0700

    fixed froala editor bugs

--- Caity/caity/app/assets/javascripts/articles.js
+++ Caity/caity/app/assets/javascripts/articles.js
@@ -1,54 +1,6 @@
-// $(document).ready(function() {
-//   $('#edit').editable({
-//     inlineMode: false,
-//     editInPopup: true,
-//     icons: {bold: {type: "font", value: "fa fa-bold"}},
-//     imageLink: false,
-//     imageMove: false,
-//     imageResize: true,
-//     imageUpload: false,
-//     paragraphy: false,
-//     pasteImage: false,
-//     plainPaste: true,
-//     textNearImage: false,
-//     videoAllowedAttributes: ["src", "width", "height"]})
-// });
+$(document).ready(function() {
+  $('#edit').editable({
+    inlineMode: false,
+})
+});
 
-// var editInPopup = $("#edit").editable("option", "editInPopup");
-
-// $("#edit").editable("option", "editInPopup", true);
-
-// var icons = $("#edit").editable("option", "icons");
-
-// $("#edit").editable("option", "icons", {bold: {type: "font", value: "fa fa-bold"}});
-
-// var imageLink = $("#edit").editable("option", "imageLink");
-
-// var imageMove = $("#edit").editable("option", "imageMove");
-
-// $("#edit").editable("option", "imageMove", false);
-
-// var imageResize = $("#edit").editable("option", "imageResize");
-// var imageUpload = $("#edit").editable("option", "imageUpload");
-
-// $("#edit").editable("option", "imageUpload", false);
-
-// var paragraphy = $("#edit").editable("option", "paragraphy");
-
-// $("#edit").editable("option", "paragraphy", false);
-
-// var pasteImage = $("#edit").editable("option", "pasteImage");
-
-// $("#edit").editable("option", "pasteImage", false);
-
-// var plainPaste = $("#edit").editable("option", "plainPaste");
-
-// $("#edit").editable("option", "plainPaste", true);
-
-// var textNearImage = $("#edit").editable("option", "textNearImage");
-
-// $("#edit").editable("option", "textNearImage", false);
-
-// var videoAllowedAttributes = $("#edit").editable("option", "videoAllowedAttributes");
-
-// $("#edit").editable("option", "videoAllowedAttributes", ["src", "width", "height"]);
